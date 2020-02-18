d96bf2658e9b4191476ce5247b8126b3368806d9
Paul Solt <PaulSolt@gmail.com>
2020-02-18 11:48:08 -0500

    Added helper popup method and updated UI

--- Tips/View Controller/LSITipViewController.m
+++ Tips/View Controller/LSITipViewController.m
@@ -10,21 +10,44 @@
 
 @interface LSITipViewController ()
 
+// Private Properties
+
+// Private IBOutlets
+
+// Private Methods
+
 @end
 
 @implementation LSITipViewController
 
 - (void)viewDidLoad {
-   
+    [super viewDidLoad];
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [self showSaveTipAlert];
 }
 
 - (void)calculateTip {
-    
+    // TODO: Calculate the tip using the values from the UI
 }
 
 - (void)updateViews {
-    
+    // TODO: Use the model data to update the views
 }
+
+- (void)saveTipNamed:(NSString *)name {
+    
+    // TODO: Save the tip to the controller and update tableview
+
+}
+
+// MARK: - IBActions
+
+
+// TODO: Connect actions for splitChanged, sliderChanged, and Save Tip button
+
 
 // MARK: - UITableViewDataSource
 
@@ -32,12 +55,46 @@
 //}
 //
 //- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-//
 //}
 
 // MARK: - UITableViewDelegate
 
 //- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+// TODO: Load the selected tip from the controller
+
 //}
+
+// MARK: - Alert Helper
+
+- (void)showSaveTipAlert {
+    UIAlertController *alert = [UIAlertController
+                                alertControllerWithTitle:@"Save Tip"
+                                message:@"What name would you like to give to this tip?"
+                                preferredStyle:UIAlertControllerStyleAlert];
+    
+    
+    [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"Tip Name:";
+    }];
+    
+    UIAlertAction *saveAction = [UIAlertAction actionWithTitle:@"Save"
+                                                         style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+        NSString *name = [[alert.textFields firstObject] text];
+        if (name.length > 0) {
+            [self saveTipNamed: name];
+        }
+    }];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:nil];
+    
+    [alert addAction:saveAction];
+    [alert addAction:cancelAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
 @end
