2261a6e7d2a942dd9ec2c3751c709f3ad496dbdb
Nick <ptnguyen1901@gmail.com>
2020-04-14 14:08:08 -0400

    Add update views

--- Tips/View Controller/LSITipViewController.m
+++ Tips/View Controller/LSITipViewController.m
@@ -16,8 +16,10 @@
 @interface LSITipViewController () <UITableViewDelegate,UITableViewDataSource>
 
 // Private Properties
-
-// Private IBOutlets
+@property (nonatomic) double total;
+@property (nonatomic) int split;
+@property (nonatomic) double percentage;
+@property (nonatomic) double tip;
 
 // Private IBOutlets
 @property (strong, nonatomic) IBOutlet UITextField *totalTextField;
@@ -60,11 +62,22 @@
 }
 
 - (void)calculateTip {
-    // TODO: Calculate the tip using the values from the UI
+    self.percentage = round(self.percentageSlider.value);
+    self.total = [self.totalTextField.text doubleValue];
+    self.split = self.splitStepper.value;
+    self.tip = self.total * (self.percentage / 100.0) / self.split;
+    NSLog(@"Tip: %f",self.tip);
+    [self updateViews];
 }
 
 - (void)updateViews {
-    // TODO: Use the model data to update the views
+    self.splitStepper.value = self.split;
+    self.percentageSlider.value = self.percentage;
+    self.totalTextField.text = [NSString stringWithFormat:@"%.2f", self.total];
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", self.tip];
+    self.splitLabel.text = [NSString stringWithFormat:@"%d", self.split];
+    // %% = % for output
+    self.percentageLabel.text = [NSString stringWithFormat:@"%0.0f%%", self.percentage];
 }
 
 - (void)saveTipNamed:(NSString *)name {
@@ -77,6 +90,35 @@
 
 
 // TODO: Connect actions for splitChanged, sliderChanged, and Save Tip button
+
+
+- (IBAction)splitChanged:(UIStepper *)sender {
+    self.split = round(self.splitStepper.value);
+    [self calculateTip];
+}
+
+
+- (IBAction)sliderChanged:(UISlider *)sender {
+    self.percentage = round(self.percentageSlider.value);
+    [self calculateTip];
+}
+
+
+- (IBAction)saveTipButtonPressed:(UIButton *)sender {
+    [self showSaveTipAlert];
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // MARK: - UITableViewDataSource
